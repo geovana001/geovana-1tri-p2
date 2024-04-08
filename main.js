4a69c97c92027d2b7743827011c4630b7a40edef
geovana <106692519+geovana001@users.noreply.github.com>
2024-04-08 09:53:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-11-30T00:00:00");
-const tempoObjetivo2 = new Date("2024-03-31T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-31T00:00:00");
 const tempoObjetivo3 = new Date("2024-07-20T00:00:00");
 const tempoObjetivo4 = new Date("2024-12-27T23:59:59");
 
